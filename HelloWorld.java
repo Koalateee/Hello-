1c3469335476478ff4cf4d846c3986778f421d7a
Koalateee <57509137+Koalateee@users.noreply.github.com>
2019-12-12 16:04:06 -0800

    12/12/19
    boolean

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class HelloWorld
+{
+   public static void main (String[]args)
+   {
+       Scanner s = new Scanner(System.in);
+       System.out.print("Get a sword?(y/n)");
+       char ans = s.next().charAt(0);
+       
+       boolean hasSword;
+       hasSword=true;
+       if(hasSword)
+       {
+           System.out.println("You have a sword");
+           System.out.println("Good luck");
+        } else
+        {
+            System.out.println("You do not have a sword");
+            System.out.println("Good luck with your bare hands");
+        }
+        System.out.println("The End");
+    }
+}
